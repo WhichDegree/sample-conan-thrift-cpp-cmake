b0fe16a5336b4f318b89cb476084c3aa000ca20d
EMCP <emcp@whichdegree.co>
2020-10-29 22:01:20 +0100

    Adding

---
+++ build_cmake/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/emcp/Dev/git/WhichDegree/sample-conan-thrift-cpp-cmake/src/gen-cpp/helloSvc.cpp" "/Users/emcp/Dev/git/WhichDegree/sample-conan-thrift-cpp-cmake/build_cmake/CMakeFiles/client.dir/src/gen-cpp/helloSvc.cpp.o"
+  "/Users/emcp/Dev/git/WhichDegree/sample-conan-thrift-cpp-cmake/src/hello_client.cpp" "/Users/emcp/Dev/git/WhichDegree/sample-conan-thrift-cpp-cmake/build_cmake/CMakeFiles/client.dir/src/hello_client.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/emcp/.conan/data/thrift/0.13.0/_/_/package/46f7c6e1bd77fc9e7c7635312426beecece66c7b/include"
+  "/Users/emcp/.conan/data/boost/1.74.0/_/_/package/076aff20c42e87a34bf7b270c852b7953bc51cd2/include"
+  "/Users/emcp/.conan/data/zlib/1.2.11/_/_/package/647afeb69d3b0a2d3d316e80b24d38c714cc6900/include"
+  "/Users/emcp/.conan/data/bzip2/1.0.8/_/_/package/75fe66fbfe61230e9fcbb361f13b6b3baa483e0a/include"
+  "/Users/emcp/.conan/data/openssl/1.1.1h/_/_/package/647afeb69d3b0a2d3d316e80b24d38c714cc6900/include"
+  "/Users/emcp/.conan/data/libevent/2.1.11/_/_/package/7ac42c52e63f1381a4948b0f2939b106f29c4ea2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_cmake/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/src/gen-cpp/helloSvc.cpp.o"
+  "CMakeFiles/client.dir/src/hello_client.cpp.o"
+  "bin/client"
+  "bin/client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
